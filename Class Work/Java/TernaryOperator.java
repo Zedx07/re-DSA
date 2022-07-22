b54a63c41435f6035de0c3107deb1e84e0870b26
Shubham Golwal <76422167+Shubham7-cell@users.noreply.github.com>
2022-07-23 03:33:30 +0530

    23/07 - stack

--- Class Work/Java/TernaryOperator.java
+++ Class Work/Java/TernaryOperator.java
@@ -1,7 +1,7 @@
 public class TernaryOperator {
     public static void main(String[] args) {
         int a = -20, b = -30, c = -500;
-        long max = ((a > b) ? (a > c) ? a : c : (b > c) ? b: c);
+        long max = ((a > b) ? (a > c) ? a : c : (b > c) ? b : c);
         System.out.println("The max number is: " + max);
         System.out.println("Yeaaa! used ternary operator :)");
     }
