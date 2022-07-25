63bdaab05c01ae4551d28c6ed26cd3d74eaeee89
Shubham Golwal <76422167+Shubham7-cell@users.noreply.github.com>
2022-07-25 10:45:51 +0530

    arranged files

---
+++ Class Work/Data Structures/6. Graphs/BFSTraversal.java
@@ -0,0 +1,79 @@
+// package Trees.Graphs;
+
+// import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BFSTraversal {
+    private int vertex;
+    private LinkedList<Integer> adj[];
+    private Queue<Integer> queue;
+    private boolean visited;
+
+    BFSTraversal(int totalNodes) {
+        vertex = totalNodes;
+        adj = new LinkedList[vertex];
+
+        for (int i = 0; i < totalNodes; i++) {
+            adj[i] = new LinkedList<>();
+        }
+        queue = new LinkedList<>();
+    }
+
+    public void insertEdge(int v, int w) {
+        adj[v].add(w);
+    }
+
+    // n -> starting node
+    void BFS(int n) {
+        boolean visited[] = new boolean[vertex];
+        int a = 0;
+        visited[n] = true;
+        queue.add(n);
+        while (queue.size() != 0) {
+            n = queue.poll();
+            System.out.println(n + " ");
+
+            // Iterator<Integer> i = adj[n].listIterator();
+            // while (i.hasNext()) {
+            // int s = i.next();
+            // if (!visited[s]) {
+            // visited[s] = true;
+            // queue.add(s);
+            // }
+            // }
+
+            for (int i = 0; i < adj[n].size(); i++) {
+                a = adj[n].get(i);
+                if (!visited[a]) {
+                    visited[a] = true;
+                    queue.add(a);
+                }
+            }
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        BFSTraversal graph = new BFSTraversal(10);
+        graph.insertEdge(0, 1);
+        graph.insertEdge(0, 2);
+        graph.insertEdge(0, 3);
+        graph.insertEdge(1, 3);
+        graph.insertEdge(2, 4);
+        graph.insertEdge(3, 5);
+        graph.insertEdge(3, 6);
+        graph.insertEdge(4, 7);
+        graph.insertEdge(4, 5);
+        graph.insertEdge(5, 2);
+        graph.insertEdge(6, 5);
+        graph.insertEdge(7, 5);
+        graph.insertEdge(7, 8);
+
+        System.out.println("BFS traversal for the given graph is: ");
+        graph.BFS(2);
+
+    }
+
+}
---
+++ Class Work/Data Structures/6. Graphs/GraphBFS.java
@@ -0,0 +1,66 @@
+import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.List;
+
+public class GraphBFS {
+    private List<List<Integer>> adj;
+    private boolean visited[];
+
+    // creation
+    GraphBFS(int allNodes) {
+        visited = new boolean[allNodes];
+        adj = new LinkedList<>();
+
+        for (int i = 0; i < allNodes; i++) {
+            adj.add(i, new LinkedList<>());
+        }
+    }
+
+    public void insertEdge(int v, int w) {
+        adj.get(v).add(w);
+        adj.get(w).add(v);
+    }
+    // till here
+
+    public void bfs(int startIndex) {
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.add(startIndex);
+        visited[startIndex] = true;
+
+        while (!queue.isEmpty()) {
+            Integer node = queue.poll();
+            System.out.println(node + " ");
+
+            List<Integer> childList = adj.get(node);
+            for (Integer child : childList) {
+                if (!visited[child]) {
+                    queue.add(child);
+                    visited[child] = true;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        GraphBFS graph = new GraphBFS(10);
+
+        graph.insertEdge(0, 1);
+        graph.insertEdge(0, 2);
+        graph.insertEdge(0, 3);
+        graph.insertEdge(1, 3);
+        graph.insertEdge(2, 4);
+        graph.insertEdge(3, 5);
+        graph.insertEdge(3, 6);
+        graph.insertEdge(4, 7);
+        graph.insertEdge(4, 5);
+        graph.insertEdge(5, 2);
+        graph.insertEdge(6, 5);
+        graph.insertEdge(7, 5);
+        graph.insertEdge(7, 8);
+
+        System.out.println("BFS traversal for the given graph is: ");
+        graph.bfs(0);
+    }
+}
